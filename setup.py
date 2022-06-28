ee2f3167c6213d2db3c202639cd72dfc34497068
Till Hoffmann <tillahoffmann@gmail.com>
2022-06-28 18:12:15 -0400

    Update requirements.

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     packages=find_packages(),
     version="0.1.0",
     install_requires=[
-        "matplotlib",
-        "numpy",
+        "doit",
     ],
     extras_require={
         "tests": [
