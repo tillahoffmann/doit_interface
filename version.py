2228e46af70cc1f31e72747a41e130fec7429f8e
Till Hoffmann <tillahoffmann@gmail.com>
2022-07-06 17:36:15 -0400

    Raise `ValueError` on misspecified version.

--- version.py
+++ version.py
@@ -2,11 +2,22 @@ import os
 import re
 
 
-ref = os.environ.get("GITHUB_REF", "")
-match = re.match(r"^/refs/tags/([\d\.]+)$", ref)
-if match:
-    version = match.group(1)
-    with open("VERSION", "w") as fp:
-        fp.write(version)
-    print(f"wrote version {version} to VERSION file")
-print(f"cannot extract version from ref {ref}")
+def __main__():
+    try:
+        ref = os.environ["GITHUB_REF"]
+    except KeyError:
+        print("GITHUB_REF environment variable is not defined")
+        return
+
+    match = re.match(r"^refs/tags/(\d+\.\d+\.\d+)$", ref)
+    if match:
+        version = match.group(1)
+        with open("VERSION", "w") as fp:
+            fp.write(version)
+        print(f"wrote version {version} to VERSION file")
+    else:
+        raise ValueError(f"cannot extract version from ref {ref}")
+
+
+if __name__ == "__main__":
+    __main__()
