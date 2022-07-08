e53279955d631506e3378d8b3bd72a2d67a61af1
Till Hoffmann <tillahoffmann@gmail.com>
2022-07-08 15:39:06 -0400

    Use improved substitution in `dodo.py`.

--- dodo.py
+++ dodo.py
@@ -12,7 +12,7 @@ with di.group_tasks("build"):
         manager(name="html", actions=["sphinx-build . docs/_build"])
         manager(name="tests", actions=["sphinx-build -b doctest . docs/_build"])
 
-    manager(basename="version", actions=[["$!", "version.py"]], file_dep=["version.py"],
+    manager(basename="version", actions=["$! version.py"], file_dep=["version.py"],
             targets=["VERSION"])
 
     with di.defaults(basename="package"):
@@ -22,15 +22,15 @@ with di.group_tasks("build"):
         except FileNotFoundError:
             version = "dev"
         archive = f"dist/doit_interface-{version}.tar.gz"
-        manager(name="sdist", actions=[["$!", "setup.py", "sdist"]], targets=[archive])
-        manager(name="check", file_dep=[archive], actions=[["twine", "check", archive]])
+        manager(name="sdist", actions=["$! setup.py sdist"], targets=[archive])
+        manager(name="check", file_dep=[archive], actions=["twine check $^"])
         manager(name="pypi-readme", file_dep=["README.rst", "setup.py"],
-                targets=["docs/_build/pypi.html"], actions=[["$!", "pypi-readme.py", "$@"]])
+                targets=["docs/_build/pypi.html"], actions=["$! pypi-readme.py $@"])
 
 with di.defaults(basename="requirements"):
     manager(name="test", targets=["test_requirements.txt"],
             file_dep=["test_requirements.in", "setup.py"],
-            actions=[["pip-compile", "-v", "-o", "$@", "test_requirements.in"]])
+            actions=["pip-compile -v -o $@ test_requirements.in"])
     manager(name="dev", targets=["requirements.txt"],
             file_dep=["requirements.in", "setup.py", "test_requirements.txt"],
-            actions=[["pip-compile", "-v", "-o", "$@", "requirements.in"]])
+            actions=["pip-compile -v -o $@ requirements.in"])
